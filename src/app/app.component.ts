3a6fae3fd1565a77adf06102c918d83965278455
Hasnen's-MAC <mac@Hasnen-Mac.local>
2017-12-02 16:24:04 +0530

    First commit

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,49 @@
+import { RegisterComponent } from './register/register.component';
 import { Component } from '@angular/core';
+
+import { Observable } from 'rxjs/Observable';
+import * as firebase from 'firebase/app';
+import {NgForm} from '@angular/forms';
+import { AngularFireDatabase, FirebaseListObservable, FirebaseObjectObservable } from 'angularfire2/database-deprecated';
+
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.scss']
 })
-export class AppComponent {
+export class AppComponent   {
   title = 'app';
+  first_name: string;
+  last_name: string;
+  e_mail: any;
+  mobile_number: number;
+  address: string;
+  city: string;
+  user: Observable<firebase.User>;
+  items: FirebaseListObservable<any[]>;
+  msgVal: any = '';
+  r: any;
+  constructor( public af: AngularFireDatabase) {
+    this.items = af.list('/users');
+}
+Send() {
+ /* this.items.push({
+    'First  Name' : this.first_name,
+    'Last Name': this.last_name,
+    'Email Addres' : this.e_mail,
+    'Phone Number' : this.mobile_number,
+    'Addrress': this.address,
+    'City': this.city
+
+});*/
+this.items.push({
+  'first': this.first_name,
+  'last': this.last_name,
+  'email': this.e_mail,
+  'phone': this.mobile_number,
+  
+});
+
+}
 }
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,16 +1,43 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
-
-
+import { RouterModule, Routes } from '@angular/router';
+import { AngularFireModule } from 'angularfire2';
 import { AppComponent } from './app.component';
+import { RegisterComponent } from './register/register.component';
+import { AngularFontAwesomeModule } from 'angular-font-awesome';
+import * as firebase from 'firebase/app';
+import { AngularFireDatabaseModule } from 'angularfire2/database-deprecated';
 
+
+
+import {FormsModule} from '@angular/forms';
+const appRoutes: Routes = [
+  { path: 'register',      component: RegisterComponent }
+];
+export const  config = {
+  apiKey: "AIzaSyDjWaLY6eTwuKf5YgPoQgPzK6cA40axDbI",
+  authDomain: "ashgen-737b9.firebaseapp.com",
+  databaseURL: "https://ashgen-737b9.firebaseio.com",
+  projectId: "ashgen-737b9",
+  storageBucket: "ashgen-737b9.appspot.com",
+  messagingSenderId: "57491185405"
+};
+firebase.initializeApp(config);
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    RegisterComponent
   ],
   imports: [
-    BrowserModule
+    RouterModule.forRoot(
+      appRoutes
+    ),
+    FormsModule,
+    BrowserModule,
+    AngularFireModule.initializeApp(config),
+    AngularFontAwesomeModule,
+    AngularFireDatabaseModule
   ],
   providers: [],
   bootstrap: [AppComponent]
